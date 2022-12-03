68ee0912a61df902e05801c8c6e6cada2f55bfae
joncoronel <59675970+joncoronel@users.noreply.github.com>
2022-12-03 02:00:01 -0800

    vh bug

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <>
+      <div style={{ height: "100vh", backgroundColor: "red" }}></div>
+      <div style={{ height: "100rem" }}></div>
+    </>
+  );
+}
